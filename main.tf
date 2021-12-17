842e1b64675c45c066dcc694291540ad41fa9a81
k-zenno <93756447+k-zenno@users.noreply.github.com>
2021-12-17 16:42:16 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ variable "region" {
 }
 
 provider "aws" {
-    access_key = var.AWS_ACCESS_KEY
-    secret_key = var.AWS_SERCRET_KEY
+    access_key = var.aws_access_key
+    secret_key = var.aws_secret_key
     region = var.region
 }
 
