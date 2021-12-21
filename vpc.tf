af5baf3dfb97d83b70ab101b97ff901d0147dc87
k-zenno <93756447+k-zenno@users.noreply.github.com>
2021-12-22 02:21:11 +0900

    variable

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@
 #VPC
 #----------------------------------------------------------
 resource "aws_vpc" "vpc_Childcare_Service" {
-    cidr_block = "10.1.0.0/16"
+    cidr_block = var.vpc_cidr_block
     instance_tenancy = "default"
     enable_dns_support = "true"
     enable_dns_hostnames = "false"
