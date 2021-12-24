f81b947c1cb63c1b8dc88fbb128c802f7145f2ef
k-zenno <93756447+k-zenno@users.noreply.github.com>
2021-12-24 11:10:07 +0900

    20211224

--- ec2.tf
+++ ec2.tf
@@ -7,18 +7,18 @@ variable "images" {
 #EC2
 #----------------------------------------------------------
 #DB用EC2作成
-resource "aws_instance" "EC2_DB_Childcare_Service" {
+resource "aws_instance" "EC2_DB_Childcare" {
     ami = var.images
     instance_type = "t2.micro"
     key_name = "zenno-test"
     vpc_security_group_ids = [
-      aws_security_group.SG_DB_Childcare_Service.id
+      aws_security_group.SG_DB_Childcare.id
     ]
     subnet_id = aws_subnet.private-a.id
     associate_public_ip_address = "true"
 
     tags = {
-        Name = "EC2_DB_Childcare_Service"
+        Name = "EC2_DB_Childcare"
         System = "CSS"
     }
 }
@@ -27,25 +27,25 @@ resource "aws_instance" "EC2_DB_Childcare_Service" {
 #ALB
 #----------------------------------------------------------
 #ALB
-resource "aws_lb" "ALB_Childcare_Service" {
-  name                       = "ALB-Childcare-Service"
-  security_groups            = [aws_security_group.SG_ALB_Childcare_Service.id]
+resource "aws_lb" "ALB_Childcare" {
+  name                       = "ALB-Childcare"
+  security_groups            = [aws_security_group.SG_ALB_Childcare.id]
   subnets                    = [aws_subnet.private-a.id,aws_subnet.private-c.id]
   internal                   = false
   enable_deletion_protection = false
   tags = {
-      Name = "ALB_Childcare_Service"
-      System = "CSS"
+      Name = "ALB_Childcare"
+      System = "CC"
   }
 }
 
 #ターゲットグループ
-resource "aws_lb_target_group" "TG_Childcare_Service" {
-  name     = "TG-Childcare-Service"
+resource "aws_lb_target_group" "TG_Childcare" {
+  name     = "TG-Childcare"
   port     = 80
   protocol = "HTTP"
   target_type = "ip"
-  vpc_id   = aws_vpc.vpc_Childcare_Service.id
+  vpc_id   = aws_vpc.vpc_Childcare.id
 
   health_check {
     interval            = 30
@@ -60,12 +60,12 @@ resource "aws_lb_target_group" "TG_Childcare_Service" {
 
 #リスナー
 resource "aws_alb_listener" "alb" {
-  load_balancer_arn = aws_lb.ALB_Childcare_Service.arn
+  load_balancer_arn = aws_lb.ALB_Childcare.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.TG_Childcare_Service.arn
+    target_group_arn = aws_lb_target_group.TG_Childcare.arn
     type             = "forward"
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
 #----------------------------------------------------------
 #VPC
 #----------------------------------------------------------
-resource "aws_vpc" "vpc_Childcare_Service" {
+resource "aws_vpc" "vpc_Childcare" {
     cidr_block = "172.26.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = "true"
     enable_dns_hostnames = "false"
     tags = {
-      Name = "VPC_Childcare_Service"
-      System = "CCS"
+      Name = "VPC_Childcare"
+      System = "CC"
     }
 }
 
@@ -19,28 +19,28 @@ resource "aws_vpc" "vpc_Childcare_Service" {
 
 #パブリックA
 resource "aws_subnet" "public-a" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     cidr_block = "10.1.1.0/24"
     availability_zone = "ap-northeast-1a"
 }
 
 #パブリックC
 resource "aws_subnet" "public-c" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     cidr_block = "10.1.2.0/24"
     availability_zone = "ap-northeast-1c"
 }
 
 #プライベートA
 resource "aws_subnet" "private-a" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     cidr_block = "10.10.1.0/24"
     availability_zone = "ap-northeast-1a"
 }
 
 #プライベートC
 resource "aws_subnet" "private-c" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     cidr_block = "10.10.2.0/24"
     availability_zone = "ap-northeast-1c"
 }
@@ -49,11 +49,11 @@ resource "aws_subnet" "private-c" {
 #----------------------------------------------------------
 #インターネットゲートウェイ
 #----------------------------------------------------------
-resource "aws_internet_gateway" "igw_Childcare_Service" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+resource "aws_internet_gateway" "igw_Childcare" {
+    vpc_id = aws_vpc.vpc_Childcare.id
     tags = {
-        Name = "igw_Childcare_Service"
-        System = "CCS"
+        Name = "igw_Childcare"
+        System = "CC"
     }
 }
 
@@ -63,23 +63,23 @@ resource "aws_internet_gateway" "igw_Childcare_Service" {
 #NATゲートウェイ
 #----------------------------------------------------------
 #NAT用EIP
-resource "aws_eip" "eip_natgw_Childcare_Service" {
+resource "aws_eip" "eip_natgw_Childcare" {
   vpc = true
 
   tags = {
-    Name = "eip_natgw_Childcare_Service"
-    System = "CCS"
+    Name = "eip_natgw_Childcare"
+    System = "CC"
   }
 }
 
 #NATゲートウェイ
-resource "aws_nat_gateway" "NATgw_Childcare_Service" {
-  allocation_id = aws_eip.eip_natgw_Childcare_Service.id
+resource "aws_nat_gateway" "NATgw_Childcare" {
+  allocation_id = aws_eip.eip_natgw_Childcare.id
   subnet_id     = aws_subnet.private-a.id
 
   tags = {
-    Name = "NATgw_Childcare_Service"
-    System = "CCS"
+    Name = "NATgw_Childcare"
+    System = "CC"
   }
 }
 
@@ -87,43 +87,43 @@ resource "aws_nat_gateway" "NATgw_Childcare_Service" {
 #ルートテーブル
 #----------------------------------------------------------
 #インターネットゲートウェイ向けルートテーブル
-resource "aws_route_table" "public-route_Childcare_Service" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+resource "aws_route_table" "public-route_Childcare" {
+    vpc_id = aws_vpc.vpc_Childcare.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw_Childcare_Service.id
+        gateway_id = aws_internet_gateway.igw_Childcare.id
     }
 }
 
 #紐づけ
 resource "aws_route_table_association" "puclic-a" {
     subnet_id = aws_subnet.public-a.id
-    route_table_id = aws_route_table.public-route_Childcare_Service.id
+    route_table_id = aws_route_table.public-route_Childcare.id
 }
 
 resource "aws_route_table_association" "puclic-c" {
     subnet_id = aws_subnet.public-c.id
-    route_table_id = aws_route_table.public-route_Childcare_Service.id
+    route_table_id = aws_route_table.public-route_Childcare.id
 }
 
 #NATゲートウェイ向けルートテーブル
-resource "aws_route_table" "private-route_Childcare_Service" {
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+resource "aws_route_table" "private-route_Childcare" {
+    vpc_id = aws_vpc.vpc_Childcare.id
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.NATgw_Childcare_Service.id
+        gateway_id = aws_nat_gateway.NATgw_Childcare.id
     }
 }
 
 #紐づけ
 resource "aws_route_table_association" "private-a" {
     subnet_id = aws_subnet.private-a.id
-    route_table_id = aws_route_table.private-route_Childcare_Service.id
+    route_table_id = aws_route_table.private-route_Childcare.id
 }
 
 resource "aws_route_table_association" "private-c" {
     subnet_id = aws_subnet.private-c.id
-    route_table_id = aws_route_table.private-route_Childcare_Service.id
+    route_table_id = aws_route_table.private-route_Childcare.id
 }
 
 
@@ -131,18 +131,18 @@ resource "aws_route_table_association" "private-c" {
 #セキュリティグループ
 #----------------------------------------------------------
 #DB用セキュリティグループ
-resource "aws_security_group" "SG_DB_Childcare_Service" {
-    name = "SG_DB_Childcare_Service"
+resource "aws_security_group" "SG_DB_Childcare" {
+    name = "SG_DB_Childcare"
     description = "Allow DB inbound traffic"
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     tags = {
-      Name = "SG_DB_Childcare_Service"
-      System = "CCS"
+      Name = "SG_DB_Childcare"
+      System = "CC"
     }
 }
 
 #ルール
-resource "aws_security_group_rule" "InboundRule_DBPort_Fargate_Childcare_Service" {
+resource "aws_security_group_rule" "InboundRule_DBPort_Childcare" {
   type        = "ingress"
   from_port   = 5432
   to_port     = 5432
@@ -151,22 +151,21 @@ resource "aws_security_group_rule" "InboundRule_DBPort_Fargate_Childcare_Service
     "10.10.1.0/24"
   ]
 
-  # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.SG_DB_Childcare_Service.id
+  security_group_id = aws_security_group.SG_DB_Childcare.id
 }
 
 #コンテナ用セキュリティグループ
-resource "aws_security_group" "SG_Fargate_Childcare_Service" {
-    name = "SG_Fargate_Childcare_Service"
+resource "aws_security_group" "SG_Fargate_Childcare" {
+    name = "SG_Fargate_Childcare"
     description = "Allow Fargate inbound traffic"
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     tags = {
-      Name = "SG_Fargate_Childcare_Service"
-      System = "CCS"
+      Name = "SG_Fargate_Childcare"
+      System = "CC"
     }
 }
 #ルール
-resource "aws_security_group_rule" "InboundRule_ALBSubnet-a_Fargate_Childcare_Service" {
+resource "aws_security_group_rule" "InboundRule_ALBSubnet-a_Childcare" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -175,12 +174,11 @@ resource "aws_security_group_rule" "InboundRule_ALBSubnet-a_Fargate_Childcare_Se
     "10.10.1.0/24"
   ]
 
-  # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.SG_Fargate_Childcare_Service.id
+  security_group_id = aws_security_group.SG_Fargate_Childcare.id
 }
 
 #ルール
-resource "aws_security_group_rule" "InboundRule_ALBSubnet-c_Fargate_Childcare_Service" {
+resource "aws_security_group_rule" "InboundRule_ALBSubnet-c_Childcare" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
@@ -189,17 +187,16 @@ resource "aws_security_group_rule" "InboundRule_ALBSubnet-c_Fargate_Childcare_Se
     "10.10.2.0/24"
   ]
 
-  # ここでweb_serverセキュリティグループに紐付け
-  security_group_id = aws_security_group.SG_Fargate_Childcare_Service.id
+  security_group_id = aws_security_group.SG_Fargate_Childcare.id
 }
 
 #ALB用セキュリティグループ
-resource "aws_security_group" "SG_ALB_Childcare_Service" {
-    name = "SG_ALB_Childcare_Service"
+resource "aws_security_group" "SG_ALB_Childcare" {
+    name = "SG_ALB_Childcare"
     description = "Allow ALB inbound traffic"
-    vpc_id = aws_vpc.vpc_Childcare_Service.id
+    vpc_id = aws_vpc.vpc_Childcare.id
     tags = {
-      Name = "SG_ALB_Childcare_Service"
-      System = "CCS"
+      Name = "SG_ALB_Childcare"
+      System = "CC"
     }
 }
